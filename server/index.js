9e09fd083f6ade0e21dbe77714dae48c7642564a
Chin Kuay <kuaychin@gmail.com>
2020-04-07 14:55:29 -0500

    created cart-controller

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ const express = require("express");
 const session = require("express-session");
 const checkForSession = require("./middlewares/checkForSession");
 const swagController = require("./controllers/swagController");
+const authController = require("./controllers/authController");
 
 const app = express();
 
@@ -20,6 +21,12 @@ app.use(
 app.use(checkForSession);
 
 // Endpoints
+//// Auth
+app.post("/api/register", authController.register);
+app.post("/api/login", authController.login);
+app.post("/api/signout", authController.signout);
+app.get("/api/user", authController.getUser);
+//// Swag
 app.get("/api/swag", swagController.read);
 
 app.listen(SERVER_PORT, () => {
