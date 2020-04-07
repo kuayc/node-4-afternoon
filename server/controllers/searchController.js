c0c6dd2177023ba07b148bec034dacf1ba09d1ab
Chin Kuay <kuaychin@gmail.com>
2020-04-07 16:05:03 -0500

    minor fixes

--- server/controllers/searchController.js
+++ server/controllers/searchController.js
@@ -1,13 +1,15 @@
-const swag = require("../models/swag");
+let swag = require("../models/swag");
+
+let search = (req, res) => {
+  const { category } = req.query;
+  if (!category) {
+    res.status(200).send(swag);
+  } else {
+    const filteredSwag = swag.filter((swag) => swag.category === category);
+    res.status(200).send(filteredSwag);
+  }
+};
 
 module.exports = {
-  search: (req, res) => {
-    const { category } = req.query;
-    if (!category) {
-      res.status(200).send(swag);
-    } else {
-      const filteredSwag = swag.filter((swag) => swag.category === category);
-      res.status(200).send(filteredSwag);
-    }
-  },
+  search,
 };
