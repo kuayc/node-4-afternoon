b8598fc6a6c85ac7a5b44ad8cf2bfa6668063329
Chin Kuay <kuaychin@gmail.com>
2020-04-07 14:25:29 -0500

    modified checkForSession and index

--- server/index.js
+++ server/index.js
@@ -1,3 +1,22 @@
 require("dotenv").config();
 const express = require("express");
 const session = require("express-session");
+const checkForSession = require("./middlewares/checkForSession");
+
+const app = express();
+
+let { SERVER_PORT, SESSION_SECRET } = process.env;
+
+app.use(express.json());
+app.use(
+  session({
+    secret: SESSION_SECRET,
+    resave: true,
+    saveUninitialized: true,
+  })
+);
+app.use(checkForSession);
+
+app.listen(SERVER_PORT, () => {
+  console.log(`Server listening on port ${SERVER_PORT}.`);
+});
